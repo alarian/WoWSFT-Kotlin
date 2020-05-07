caad6baff688fc5d31bc616043bf93931413ad81
EdibleBug <20157185+EdibleBug@users.noreply.github.com>
2020-05-07 13:27:00 +0900

    - Click to remove specific artillery

--- WoWSFT-App/src/main/resources/static/js/pages/arty.js
+++ WoWSFT-App/src/main/resources/static/js/pages/arty.js
@@ -1,11 +1,22 @@
 var index = 0;
-var data1;
-var materialChart1;
 var chartDiv1 = document.getElementById('chart_div1');
-var data2;
-var materialChart2;
 var chartDiv2 = document.getElementById('chart_div2');
+var data1;
+var data2;
+var materialChart1;
+var materialChart2;
+var chart1Title = 'Penetration (mm) and Flight Time (s) over distance'
+var chart2Title = 'Penetration (mm) and Impact Angle (deg) over distance'
+var penetration = 'Penetration (mm)';
+var flight = 'Flight Time (s)';
+var impact = 'Impact Angle (deg)';
 var waitTime = 1500;
+var materialOptionsBase = {
+    // width: 800,
+    height: 600,
+    hAxis: {format: '#,###.###', viewWindow: {min: 0}},
+    vAxis: {format: '#,###.###'}
+};
 
 $(function () {
     doInit();
@@ -62,15 +73,15 @@ function changeArty(shipId)
     $('#' + shipId).addClass('show');
 }
 
-$('#addSelect').on('click', function () {
-    var selectedNation = $('#nationSelect option:selected').val();
-    var selectedShipType = $('#' + selectedNation + ' option:selected').val();
-    var selectedShip = $('.' + selectedNation + '.' + selectedShipType).val();
-    var shipId = $('.' + selectedNation + '.' + selectedShipType + ' option:selected').attr('ship-id');
-    var artyName = $('#' + shipId  + ' option:selected').val();
-    var artyId = $('#' + shipId  + ' option:selected').attr('arty-id');
-
-    var url = '/arty?index=' + shipId + '&artyId=' + artyId;
+$(document).on('click', '#addSelect', function () {
+    var selectedNation = $('#nationSelect option:selected').val(),
+        selectedShipType = $('#' + selectedNation + ' option:selected').val(),
+        selectedShip = $('.' + selectedNation + '.' + selectedShipType).val(),
+        shipId = $('.' + selectedNation + '.' + selectedShipType + ' option:selected').attr('ship-id'),
+        selectedArty = $('#' + shipId  + ' option:selected'),
+        artyName = selectedArty.val(),
+        artyId = selectedArty.attr('arty-id'),
+        url = '/arty?index=' + shipId + '&artyId=' + artyId;
 
     $.ajax({
         url: url,
@@ -108,6 +119,7 @@ $('#addSelect').on('click', function () {
                 }
                 drawMaterialChart(data1, materialChart1, chartDiv1, selectedShip + ' ' + artyName, list1, 'Penetration (mm)', 'Flight Time (s)');
                 drawMaterialChart(data2, materialChart2, chartDiv2, selectedShip + ' ' + artyName, list2, 'Penetration (mm)', 'Impact Angle (deg)');
+                $('#chart_list').append('<button class="button_arty select_arty" data-index="' + index + '">' + selectedShip + ' ' + artyName + '</button>');
                 index++;
 
                 // window.setTimeout(function() {
@@ -135,35 +147,12 @@ function drawMaterialChart(data, materialChart, chartDiv, artyName, list, y1, y2
     }
     data.addRows(list);
 
-    materialChart = new google.charts.Line(chartDiv);
+    // materialChart = new google.charts.Line(chartDiv);
 
-    var materialOptions = {
-        chart: {
-            title: y1 + ' and ' + y2 + ' over distance'
-        },
-        // width: 800,
-        height: 600,
-        series: tempSeries,
-        axes: {
-            y: {
-                'y1': {
-                    label: y1
-                },
-                'y2': {
-                    label: y2
-                }
-            }
-        },
-        hAxis: {
-            format: '#,###.###',
-            viewWindow: {
-                min: 0
-            }
-        },
-        vAxis: {
-            format: '#,###.###'
-        }
-    };
+    var materialOptions = materialOptionsBase
+    materialOptions.chart = {title: y1 + ' and ' + y2 + ' over distance'};
+    materialOptions.series = tempSeries;
+    materialOptions.axes = {y: {'y1': {label: y1}, 'y2': {label: y2}}}
     materialChart.draw(data, google.charts.Line.convertOptions(materialOptions));
 }
 
@@ -173,17 +162,69 @@ function drawStuff()
     data1.addColumn('number', 'Distance (m)');
     data2 = new google.visualization.DataTable();
     data2.addColumn('number', 'Distance (m)');
+    materialChart1 = new google.charts.Line(chartDiv1);
+    materialChart2 = new google.charts.Line(chartDiv2);
 }
 
 function doInit()
 {
-    google.charts.load('current', {'packages':['corechart', 'scatter', 'line']});
+    google.charts.load('current', {'packages': ['corechart', 'scatter', 'line']});
     google.charts.setOnLoadCallback(drawStuff);
 }
 
-$('#resetChart').on('click', function () {
+function resetChart()
+{
     $('#chart_div1').html('');
     $('#chart_div2').html('');
+    $('#chart_list').html('');
     index = 0;
     doInit();
+}
+
+$('#resetChart').on('click', function () {
+    resetChart()
 });
+
+function removeData(idx)
+{
+    data1.Ff.splice(idx * 2 + 1, 2);
+    data2.Ff.splice(idx * 2 + 1, 2);
+    for (var i = 0; i < data1.eg.length; i++) {
+        data1.eg[i].c.splice(idx * 2 + 1, 2);
+        data2.eg[i].c.splice(idx * 2 + 1, 2);
+    }
+    index = index - 1;
+
+    var tempSeries = {};
+    for (var i = 0; i <= index; i++) {
+        tempSeries[i * 2] = {axis: 'y1', targetAxisIndex: 0};
+        tempSeries[i * 2 + 1] = {axis: 'y2', targetAxisIndex: 1};
+    }
+    var materialOptions = materialOptionsBase;
+    materialOptions.chart = {title: penetration + ' and ' + flight + ' over distance'};
+    materialOptions.series = tempSeries;
+    materialOptions.axes = {y: {'y1': {label: penetration }, 'y2': {label: flight}}}
+    materialChart1.draw(data1, google.charts.Line.convertOptions(materialOptions));
+
+    materialOptions.chart = {title: penetration + ' and ' + impact + ' over distance'};
+    materialOptions.axes = {y: {'y1': {label: penetration}, 'y2': {label: impact}}}
+    materialChart2.draw(data2, google.charts.Line.convertOptions(materialOptions));
+}
+
+$(document).on('click', '.button_arty.select_arty', function () {
+    var $this = $(this),
+        position = parseInt($this.attr('data-index')),
+        $buttons = $('.button_arty.select_arty'),
+        count = $buttons.length;
+
+    $this.remove();
+    if (count <= 1) {
+        resetChart();
+    } else {
+        removeData(position);
+
+        for (var i = position + 1; i < count; i++) {
+            $buttons.eq(i).attr('data-index', $buttons.eq(i).attr('data-index') - 1);
+        }
+    }
+})
